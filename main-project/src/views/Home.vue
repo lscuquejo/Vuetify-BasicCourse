20a2ce7b440e54f82463b307eefb48bfd1eb32e8
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-07-22 14:50:27 +0100

    Adding git stuff

--- main-project/src/views/Home.vue
+++ main-project/src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A asperiores, consequatur distinctio ducimus et expedita inventore laudantium minima molestias officiis omnis optio provident quaerat sed similique, soluta, sunt unde voluptate!</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem consectetur eaque error facilis ipsa maxime modi, nam necessitatibus officiis optio perferendis provident quaerat temporibus ut vero voluptate. Distinctio, suscipit?</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This is a subheading</p>
   </div>
 </template>
 
