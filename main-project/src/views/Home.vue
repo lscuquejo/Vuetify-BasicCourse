8acfebc38d1a1560209c0d7bd576089f26dd9986
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-07-23 09:07:14 +0100

    Episode 5 Completed

--- main-project/src/views/Home.vue
+++ main-project/src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A asperiores, consequatur distinctio ducimus et expedita inventore laudantium minima molestias officiis omnis optio provident quaerat sed similique, soluta, sunt unde voluptate!</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem consectetur eaque error facilis ipsa maxime modi, nam necessitatibus officiis optio perferendis provident quaerat temporibus ut vero voluptate. Distinctio, suscipit?</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">smaller display</h4>
-    <p class="headline">This is a headline</p>
-    <p class="subheading">This is a subheading</p>
+
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn color="pink">click me</v-btn>
+    <v-btn flat color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>EMAIL ME</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>EMAIL ME</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>EMAIL ME</span>
+      <v-icon right large>email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
