de52c2064fb8b9be7268bbb1e92dca39cf9a186f
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-07-23 09:21:34 +0100

    episode-6 completed

--- main-project/src/views/Home.vue
+++ main-project/src/views/Home.vue
@@ -1,30 +1,9 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn color="pink">click me</v-btn>
-    <v-btn flat color="pink">click me</v-btn>
-
-    <v-btn depressed class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>EMAIL ME</span>
-    </v-btn>
-
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>EMAIL ME</span>
-    </v-btn>
-
-    <v-btn depressed large class="pink white--text">
-      <span>EMAIL ME</span>
-      <v-icon right large>email</v-icon>
-    </v-btn>
-
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>favorite</v-icon>
-    </v-btn>
-
+    <v-btn class="hidden-md-and-down">click me</v-btn>
+    <v-btn class="hidden-md-and-up">click me tho</v-btn>
+    <v-btn class="hidden-sm-only">click me thu</v-btn>
   </div>
 </template>
 
