dee3c34decf5f29dcd649f0b1d2da5466f984b4a
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-07-23 10:52:11 +0100

    episode-8 completed.

--- main-project/src/components/NavBar.vue
+++ main-project/src/components/NavBar.vue
@@ -1,6 +1,9 @@
 <template>
     <nav>
         <v-toolbar flat app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer">
+
+            </v-toolbar-side-icon>
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">soo</span>
                 <span>gud</span>
@@ -11,11 +14,20 @@
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
         </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app class="indigo" temporary>
+            <p>test</p>
+        </v-navigation-drawer>
+
     </nav>
 </template>
 
 <script>
     export default {
-
+        data() {
+            return {
+                drawer: false
+            }
+        }
     }
 </script>
