3161689eef41ae209e97e496e318cf43897432b4
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-07-31 09:31:57 +0100

    episode-17 completed.

--- main-project/src/views/Dashboard.vue
+++ main-project/src/views/Dashboard.vue
@@ -1,30 +1,45 @@
 <template>
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
+
     <v-container class="my-5">
+
+      <v-layout row class="mb-3">
+        <v-btn small flat color="grey" @click="sortBy('title')">
+          <v-icon left small>folder</v-icon>
+          <span class="caption text-lowercase">By project Title</span>
+        </v-btn>
+        <v-btn small flat color="grey" @click="sortBy('person')">
+          <v-icon left small>person</v-icon>
+          <span class="caption text-lowercase">By Person</span>
+        </v-btn>
+      </v-layout>
+
       <v-card flat v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
-            <div class="caption grey--text">Project Title</div>
+            <div class="caption grey--text">Project title</div>
             <div>{{ project.title }}</div>
           </v-flex>
-          <v-flex xs6 sm4 mdd2>
+          <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
             <div>{{ project.person }}</div>
           </v-flex>
-          <v-flex xs6 sm4 mdd2>
+          <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Due by</div>
-            <div>1{{ project.due }}</div>
+            <div>{{ project.due }}</div>
           </v-flex>
-          <v-flex xs2 sm4 mdd2>
-            <div>
-              <v-chip small :class="`${project.status} white--text caption my-2`">{{ project.status }}</v-chip>
+          <v-flex xs2 sm4 md2>
+            <div class="right">
+              <v-chip small :class="`${project.status} white--text my-2 caption`">{{ project.status }}</v-chip>
             </div>
           </v-flex>
         </v-layout>
         <v-divider></v-divider>
       </v-card>
+
     </v-container>
+
   </div>
 </template>
 
@@ -38,6 +53,11 @@
           { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
           { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         ]
+      }
+    },
+    methods: {
+      sortBy(prop){
+        this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
       }
     }
   }
